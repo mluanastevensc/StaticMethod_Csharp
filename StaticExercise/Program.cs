c2d88575f7ae9a0b601baf85d1ba269f892451db
Michelle Stevens <stevens.michellec@gmail.com>
2021-03-30 21:32:22 -0700

    static

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var Far = TempConverter.CelsiusToFahrenheit(44);
+            Console.WriteLine(celsius);
+            Console.WriteLine(Far);
+        }
+        
+      
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double Fahrenheit)
+        {
+            return (Fahrenheit-32) / 1.8;
+           
+        }
+        public static double CelsiusToFahrenheit(double Celsius)
+        {
+            return (Celsius * 9) / 37;
+           
+        }
+        
+
+        
+       
+    }
+}
